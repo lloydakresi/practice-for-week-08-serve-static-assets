8e911ed40c0b0b9a93e6eb65b487b4e9304369c0
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-15 22:10:12 +0000

    completed

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  res.statusCode = 200;
+  const fileHtml = fs.readFileSync("index.html");
+
+  if(req.url.startsWith("/static")){
+
+  let path = req.url.split("/");
+  const folder = path[1];
+  const asset = path[2];
+
+  let extension = asset.split(".")[1];
+
+
+  if(extension === "css") res.setHeader("Content-Type", "text/css");
+
+  if(extension === "jpeg") res.setHeader("Content-Type", "image/jpeg");
+
+  let fileOther = fs.readFileSync(`./${folder}/${asset}`);
+  return res.end(fileOther);
+
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  return res.end(fileHtml);
+
+
 });
 
 const port = 5000;
